dd901d584e04562431e7db246a59990431430120
zumatrahia <84982254+zumatrahia@users.noreply.github.com>
2021-06-01 21:28:11 +0300

    Clean Code Improvements

--- 001-Multiplesof3and5.kt
+++ 001-Multiplesof3and5.kt
@@ -1,13 +1,17 @@
-
 /** If we list all the natural numbers below 10 that are multiples of 3 or 5,
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. **/
 
-fun main(){
-    val number = 999
+fun main() {
+    println("Enter Number")
+    val number = readLine().toString().toInt()
+    sumOfMod3or5(number)
+}
+
+fun sumOfMod3or5(number: Int) {
     var result = 0
     var list = mutableListOf<Int>()
-    for (i in 0..number){
+    for (i in 0 until number) {
         if (i % 3 == 0 || i % 5 == 0)
             list.add(i)
     }
